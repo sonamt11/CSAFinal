34ddc9c44fd72b785ffa9650a8edef2e3384af0f
Sonam <sthukral6331@bths.edu>
2023-05-28 15:49:10 -0400

    Initial commit

---
+++ src/GUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class GUI {
+
+    private JPanel mainPanel;
+    private String catName;
+    private JTextField typeTheNameYouTextField;
+    private JButton startGameButton;
+
+    public GUI() {
+        catName = typeTheNameYouTextField.getText();
+
+
+    }
+
+
+}
